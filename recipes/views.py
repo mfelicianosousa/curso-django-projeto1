330a180cdb84c2ca7a4d0d27a90d30f45f62f219
Marcelino Feliciano de Sousa <marcelino.feliciano@hotmail.com>
2022-10-22 15:56:39 -0400

    Removido Contact e Sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,7 @@
-from django.http import HttpResponse
+
 from django.shortcuts import render
 
 # Create your views here.
 
 def home(request):
     return render(request,'recipes/home.html')
-
-
-def contato(request):
-    return render(request, 'me-apague/temp.html')
-
-
-def sobre(request):
-    return HttpResponse("Sobre one")
