ef7e2c5d713308b250c6511221d2c9a071ccc0aa
Marcelino Feliciano de Sousa <marcelino.feliciano@hotmail.com>
2022-11-03 21:26:34 -0400

    Django Model and ORM (Object Relational Mapper)

--- recipes/models.py
+++ recipes/models.py
@@ -1,4 +1,5 @@
 from tkinter.tix import MAX
+
 from django.contrib.auth.models import User
 from django.db import models
 
@@ -6,6 +7,9 @@ from django.db import models
 # Create your models here.
 class Category( models.Model):
     name = models.CharField(max_length=65)
+
+    def __str__(self):
+        return self.name
 
 class Recipe( models.Model):
     title = models.CharField( max_length=65)
@@ -15,15 +19,20 @@ class Recipe( models.Model):
     preparation_time_unit = models.CharField( max_length=65)
     servings = models.IntegerField()
     servings_unit = models.CharField( max_length=65)
-    preparation_step = models.CharField(max_length=1024 )
+    preparation_step = models.TextField()
     description_step_is_html = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_published =  models.BooleanField(default=False)
-    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d')
+    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d',blank=True,default='')
     category = models.ForeignKey(
-        Category, on_delete=models.SET_NULL, null=True
+        Category, on_delete=models.SET_NULL, null=True, blank=True,
+        default=None,
     )
     author = models.ForeignKey(
-        User, on_delete=models.SET_NULL, null=True
+        User, on_delete=models.SET_NULL, null=True, blank=True,
+        default=None,
     )
+
+    def __str__(self):
+        return self.title
