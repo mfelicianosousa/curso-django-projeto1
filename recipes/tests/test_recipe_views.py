4052d0f5d020b47847ebc67105dec44f700fe0f3
Marcelino Feliciano de Sousa <marcelino.feliciano@hotmail.com>
2022-11-27 21:22:36 -0400

    Primeira fixture para teste único

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -2,9 +2,11 @@ from django.test import TestCase
 from django.urls import resolve, reverse
 
 from recipes import views
+from recipes.models import Category, Recipe, User
 
 
 class RecipeViewsTest( TestCase):
+  
     def test_recipe_home_view_funcions_is_correct(self):
         view = resolve(reverse('recipes:home'))
         self.assertIs(view.func, views.home)
@@ -20,6 +22,31 @@ class RecipeViewsTest( TestCase):
     def test_recipe_home_template_shows_no_recipes_found_if_no_recipes(self):
         response = self.client.get(reverse('recipes:home'))
         self.assertIn('<h1> No recipes found here </h1>',response.content.decode('utf-8'))
+
+    def test_recipe_home_template_loads_recipes(self):
+        category = Category.objects.create(name='Category')
+        author = User.objects.create_user(
+            first_name='user',
+            last_name='name',
+            username='username',
+            password='123456',
+            email='username@email.com',
+        )
+        recipe = Recipe.objects.create(
+            category=category,
+            author=author,
+            title='Recipe Title',
+            description='Recipe Description',
+            slug='recipe-slug',
+            preparation_time=10,
+            preparation_time_unit='Minutos',
+            servings=5,
+            servings_unit='Porções',
+            preparation_steps='Recipe Preparation Steps',
+            preparation_steps_is_html=False,
+            is_published=True,
+        )
+        assert 1 == 1
 
     def test_recipe_category_view_funcions_is_correct(self):
         view = resolve(reverse('recipes:category', kwargs={'category_id':1000}))
